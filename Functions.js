64ef8c430ce53969b2a82246912df1cd4d8899d3
Mohamed Ahmed <89504279+mohamed4822@users.noreply.github.com>
2024-12-24 12:39:38 +0200

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -11,37 +11,7 @@
     setInterval(updateDate, 1000);
 
 
-    //Rating function
-    document.addEventListener("DOMContentLoaded", () => {
-      const stars = document.querySelectorAll(".star");
-      const feedback = document.getElementById("feedback");
-      let lastRating = null; 
-  
-      stars.forEach(star => {
-          star.addEventListener("click", () => {
-              lastRating = star.getAttribute("data-value"); 
-              highlightStars(lastRating);
-              feedback.innerText = `You rated us ${lastRating} out of 5. Thank you!`;
-              alert(feedback.innerText);
-          });
-  
-          star.addEventListener("mouseover", () => {
-              const rating = star.getAttribute("data-value");
-              highlightStars(rating);
-          });
-  
-          star.addEventListener("mouseout", () => {
-              highlightStars(lastRating); 
-          });
-      });
-  
-      function highlightStars(rating) {
-          stars.forEach(star => {
-              if (rating && star.getAttribute("data-value") <= rating) {
-                  star.classList.add("selected");
-              } else {
-                  star.classList.remove("selected");
-              }
+
             
           });
       }
